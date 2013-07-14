83b6ecb4513d96e9f9b8e74d9772b0cb964d2332
borrel <borrel@cpm22>
2013-07-14 16:18:00 +0300

    add barplot distance neighbor

--- barplotNeighbor.R
+++ barplotNeighbor.R
@@ -1,5 +1,4 @@
 #!/usr/bin/env Rscript
-
 source("tool.R")
 
 
@@ -11,53 +10,25 @@ source("tool.R")
 
 args <- commandArgs(TRUE)
 pathData = args[1]
-pathData_distance = args[2]
-study_type = args[3]
-
-data = read.table(pathData , sep = "\t", header = TRUE)
-data_distance = read.table(pathData_distance , sep = "\t")
-
-data_distance = data_distance[,-1]
+study_type = args[2]
 
 
-color = defColor (colnames (data))
-
-png(filename=paste(pathData,".png",sep = ""),width=as.integer(600))
-par(xpd=T, mar=par()$mar+c(0,0,0,10)) 
-barplot(t(data), ylim = c(0,1), main=paste("Distribution neighbors ", study_type, sep = ""), ylab="Frequencies", xlab = "", col=color, space=0.6, cex.axis=1, las=1, cex=1, axes = TRUE)
-legend("right",legend=colnames(data), fill=color,inset=c(-0.2,0))
-dev.off()
-
-png(filename=paste(pathData_distance ,"_type.png",sep = ""),width=as.integer(800), heigh = 1000)
+png(filename=paste(pathData ,".png",sep = ""), width=as.integer(800), heigh = 1000)
 par(mfrow = c(3,1))
 
-plot(density(as.double(t(data_distance[1,]))), ylim = c(1,6))
-for (class in names(color)){
-	print (class)
-	try (lines (density(as.double(t(data_distance[3,which(data_distance[4,] == class)]))), col = color[class]))
-}
 
-plot(density(as.double(t(data_distance[3,]))), ylim = c(1,6))
-for (class in names(color)){
-	try (lines (density(as.double(t(data_distance[3,which(data_distance[4,] == class)]))), col = color[class]))
-}
+for (i in 1:3){
+	d = read.table(paste(pathData, "_",i, sep = ""), sep = "\t", header = TRUE)
+	print (d)
+	d = d[,-1]	
 
-plot(density(as.double(t(data_distance[5,]))), ylim = c(1,6))
-for (class in names(color)){
-	try (lines (density(as.double(t(data_distance[5,which(data_distance[6,] == class)]))), col = color[class]))
-}
+	color = defColor (colnames (d))
+	barplot(t(d), ylab= "Number of occurences", col = color, xlab = "distance (Å)", names.arg = rownames (d),space = 0.5, main =i)
 
+	if (i == 2){
+		legend("left", legend=colnames(d), fill=color )
+	}
+
+}
 
 dev.off()
-
-
-png(filename=paste(pathData_distance ,".png",sep = ""),width=as.integer(800), heigh = 1000)
-par(mfrow = c(3,1))
-
-plot(density(as.double(t(data_distance[1,]))), xlim = c(1,5))
-plot(density(as.double(t(data_distance[3,]))), xlim = c(1,5))
-plot(density(as.double(t(data_distance[5,]))), xlim = c(1,5))
-
-
-dev.off()
-
